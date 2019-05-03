3f6dddb635d7d25b42233f0db21460b1c40685a3
harley <harley.veillette@gmail.com>
2019-05-03 10:53:39 -0300

    Add question to print a list of all cars

--- NBCC-WMAD-RWProps2/Program.cs
+++ NBCC-WMAD-RWProps2/Program.cs
@@ -15,7 +15,8 @@ namespace NBCC_WMAD_Console
         private static void LoadMenu()
         {
             Menu.menuDictionary.Add(0, "Exit");
-            Menu.menuDictionary.Add(1, "About the Console");            
+            Menu.menuDictionary.Add(1, "About the Console");
+            Menu.menuDictionary.Add(2, "Car builder");
         }
 
         static void Main(string[] args)
@@ -32,6 +33,9 @@ namespace NBCC_WMAD_Console
                     case 1:
                         AboutThisApp();
                         break;
+                    case 2:
+                        CarBuilder();
+                        break;
                     //Add more options in the menu here
                 }
             }
@@ -39,7 +43,51 @@ namespace NBCC_WMAD_Console
             Out.P("Press any key to exit.");
             Console.ReadLine();
         }
-                
+        //store the cars created in the list
+        static List<car> myCars = new List<car>();
+
+        private static void CarBuilder()
+        {
+            string userCarType = In.GetString("Provide a car type (i.e car, truck, van) \n");
+            int userNumberOfDoors = In.GetInt("Provide number of doors:");
+            int userCarSpeed = In.GetInt("Provide car speed:");
+            string question = In.GetString("Do you want to print list of all cars? y/n");
+
+            car car = new car();
+            car.CarType = userCarType;
+            car.NumberOfDoors = userNumberOfDoors;
+            car.Speed = userCarSpeed;
+            
+
+            Out.P("*************************");
+            Out.P($"The car created is: \n" +
+                $"Type: {car.CarType} \n" +
+                $"Doors: {car.NumberOfDoors} \n" +
+                $"Speed: {car.Speed} \n" +
+                $"Price: {car.Price.GetValueOrDefault()}  \n");
+            Out.P("*************************");
+
+            //print 
+            myCars.Add(car);
+
+            Out.P($"I have a car count in my list of: {myCars.Count}");
+
+            Out.P("Do you want to list all cars y/n");
+
+            if(question == "y")
+            {
+                foreach (var item in myCars)
+                {
+                    Out.P($"Car type: {item.CarType} Numb of doors: {item.NumberOfDoors} Speed: {item.Speed} Price: {item.Price.GetValueOrDefault().ToString("c")} \n");
+                }
+            }
+            
+            //prompt the user "Do you want to list all cars" yes or no
+            //if user enter yes => loop the list of cars => print each car with all
+            //properties
+            //if user enter no => don't print
+        }
+
         #region [My Functionality]
 
         /*
---
+++ NBCC-WMAD-RWProps2/car.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBCC_WMAD_Console
+{
+    public class car
+    {
+        private int? speed;
+        private decimal? price2;
+
+        //same as private decimal? price
+        private Nullable<decimal> price;
+        private int numberOfDoors;
+        private string carType;
+
+        public int? Year { get; set; }
+
+
+        public int? Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                speed = value;
+            }
+        }
+
+                
+        public Nullable<decimal> Price
+        {
+            get {return price;}
+            set {price = value;}
+        }
+        public decimal? Price2
+        {
+            get { return price2; }
+            set { price2 = value; }
+        }
+
+        public int NumberOfDoors
+        {
+            get
+            {
+                return numberOfDoors;
+            }
+            set
+            {
+                if (value < 10)
+                {
+                    numberOfDoors = value;
+                }
+            }
+        }
+
+        public string CarType
+        {
+            get
+            {
+                return carType;
+            }
+            set
+            {
+                carType = value;
+            }
+        }
+    }
+}
